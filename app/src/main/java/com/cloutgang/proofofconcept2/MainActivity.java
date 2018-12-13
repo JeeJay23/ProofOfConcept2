ef3affde99aa613aa36a354070cd0019676cef02
Jader Vulcano <jvjader@gmail.com>
2018-12-13 10:35:13 +0100

    Init project

---
+++ app/src/main/java/com/cloutgang/proofofconcept2/MainActivity.java
@@ -0,0 +1,90 @@
+package com.cloutgang.proofofconcept2;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.cloutgang.proofofconcept2.MESSAGE";
+
+    private DatabaseReference mDataBase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDataBase = FirebaseDatabase.getInstance().getReference();
+    }
+
+    public void sendMessage(View view)
+    {
+        // create intent
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        // get the EditText
+        EditText editText = (EditText) findViewById(R.id.editText2);
+        // get string from the EditText
+        String message = editText.getText().toString();
+        // pass along string to new activity
+        intent.putExtra(EXTRA_MESSAGE, message);
+
+        startActivity(intent);
+    }
+
+    public void SendToFireBase(View view)
+    {
+        String key = ((EditText)findViewById(R.id.editText)).getText().toString();
+        String value = ((EditText)findViewById(R.id.editText2)).getText().toString();
+
+        // quick way of acessing child objects
+//        mDataBase.child(key).setValue(value);
+
+        // cleaner way
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference(key);
+        myRef.setValue(value);
+
+
+        // Create hasmap for more complicated data
+//        HashMap<String, String> dataMap = new HashMap<String, String>();
+//        dataMap.put("Name", key);
+//        dataMap.put("Email", value);
+
+        // Use push for a generated key
+//        mDataBase.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                if (task.isSuccessful())
+//                {
+//                    Toast.makeText(
+//                            MainActivity.this,
+//                            "Data Saved!",
+//                            Toast.LENGTH_LONG).show();
+//
+//                }
+//                else
+//                {
+//                    Toast.makeText(
+//                            MainActivity.this,
+//                            "Error, something went wrong",
+//                            Toast.LENGTH_LONG).show();
+//
+//                }
+//            }
+//        });
+    }
+
+
+}
