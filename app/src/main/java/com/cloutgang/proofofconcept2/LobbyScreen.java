440ff2d96287768450d1d859b3db653d0dca2597
Jader Vulcano <jvjader@gmail.com>
2018-12-20 13:41:01 +0100

    created branch

--- app/src/main/java/com/cloutgang/proofofconcept2/LobbyScreen.java
+++ app/src/main/java/com/cloutgang/proofofconcept2/LobbyScreen.java
@@ -2,6 +2,9 @@ package com.cloutgang.proofofconcept2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class LobbyScreen extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class LobbyScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby_screen);
+    }
+
+    public void AddTextView(View view)
+    {
+        LinearLayout layout = findViewById(R.id.linearLayout);
+
+        TextView txtView = new TextView(this);
+        txtView.setText("This is a test view");
+
+        layout.addView(txtView);
     }
 }
