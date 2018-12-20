42ad19beae1b8ab85821ddb1bf961bd3741f1cf6
Jader Vulcano <jvjader@gmail.com>
2018-12-20 13:16:19 +0100

    Added new clean activity

---
+++ app/src/main/java/com/cloutgang/proofofconcept2/LobbyScreen.java
@@ -0,0 +1,13 @@
+package com.cloutgang.proofofconcept2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LobbyScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby_screen);
+    }
+}
