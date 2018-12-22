49bce45e60751f4971969100cad240209b530326
Tim <timchermin@hotmail.com>
2018-12-22 21:18:16 +0100

    Comments toegevoegd

--- app/src/main/java/com/cloutgang/proofofconcept2/SignUpScreen.java
+++ app/src/main/java/com/cloutgang/proofofconcept2/SignUpScreen.java
@@ -14,6 +14,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 public class SignUpScreen extends AppCompatActivity implements View.OnClickListener {
 
@@ -27,6 +28,8 @@ public class SignUpScreen extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_screen);
 
+
+
         findViewById(R.id.textViewSignUp).setOnClickListener(this);
 
         editTextUsername = (EditText) findViewById(R.id.editTextUsernameSignUp);
@@ -39,42 +42,53 @@ public class SignUpScreen extends AppCompatActivity implements View.OnClickListe
 
     }
 
-
+    //register the user
     private void registerUser(){
         String username = editTextUsername.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
 
+        //check if the username is filled in
         if(username.isEmpty()){
             editTextUsername.setError("Email is required");
             editTextUsername.requestFocus();
             return;
         }
 
+        //check if the email is legit
         if(!Patterns.EMAIL_ADDRESS.matcher(username).matches()){
             editTextUsername.setError("Please enter a valid email");
             editTextUsername.requestFocus();
             return;
         }
 
-
+        //check if the password is filled in
         if(password.isEmpty()){
             editTextPassword.setError("Password is required");
             editTextPassword.requestFocus();
             return;
         }
 
+        //check if the password is longer than 6 character
         if(password.length()<6){
-            editTextPassword.setError("The minimum lenght of a password should be 6 characters");
+            editTextPassword.setError("The minimum length of a password should be 6 characters");
             editTextPassword.requestFocus();
             return;
         }
 
-
+        //when everything is filled in correctly, send the username and password to Firebase
         mAuth.createUserWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+                //check if the registration succeeded
                 if(task.isSuccessful()){
                     Toast.makeText(getApplicationContext(), "User Registered", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //check if the email is already registered
+                    if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                        Toast.makeText(getApplicationContext(), "This email is already registered", Toast.LENGTH_SHORT).show();
+                    }
+
                 }
             }
         });
@@ -82,20 +96,21 @@ public class SignUpScreen extends AppCompatActivity implements View.OnClickListe
     }
 
 
-
+    //when something is clicked this will trigger
     @Override
     public void onClick(View v) {
+        //check for the clicked object
         switch(v.getId()){
+            //if the clicked object was the textViewSignUp
             case R.id.textViewSignUp:
-
+                //go to the LoginScreen
                 startActivity(new Intent(this, LoginScreen.class));
-
                 break;
 
+            //if the clicked object was the BtnSignUp
             case R.id.BtnSignUp:
                 registerUser();
                 break;
-
         }
     }
 }
