437d2dc3e457ad29ff4a192c2072d7fe00e3bf75
ZeusKingOfTheGods <timchermin@hotmail.com>
2018-12-31 12:49:09 +0100

    begin gemaakt aan het profileScreen layout basics klaar

--- app/src/main/java/com/cloutgang/proofofconcept2/ProfileScreen.java
+++ app/src/main/java/com/cloutgang/proofofconcept2/ProfileScreen.java
@@ -1,13 +1,26 @@
 package com.cloutgang.proofofconcept2;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class ProfileScreen extends AppCompatActivity {
+
+
+    ImageView imageView;
+    EditText editText;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_screen);
+
+        editText = (EditText) findViewById(R.id.editTextDisplayName);
+
+
     }
 }
