4e97ce3dcb65dd5682c1813d0c3995e6f120bf29
Tim <timchermin@hotmail.com>
2019-01-13 17:33:09 +0100

    Toolbar bij LobbyScreen toegevoegd

--- app/src/main/java/com/cloutgang/proofofconcept2/LobbyScreen.java
+++ app/src/main/java/com/cloutgang/proofofconcept2/LobbyScreen.java
@@ -1,9 +1,14 @@
 package com.cloutgang.proofofconcept2;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -42,6 +47,12 @@ public class LobbyScreen extends AppCompatActivity {
         txtIngredient = findViewById(R.id.txtIngredients);
         guestScroll = findViewById(R.id.GuestScrollView);
         guestLayout = findViewById(R.id.GuestList);
+
+        //toolbar stuff
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("FeedMe");
+
 
         Bundle b = getIntent().getExtras();
 
@@ -125,5 +136,45 @@ public class LobbyScreen extends AppCompatActivity {
             }
 
         }
+    }
+
+
+
+
+
+
+    //run this when the Menu for the logout button is created (see app/res/menu)
+    //this adds the menu to the activity
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+
+    //this happens when you click on the options icon in the menu
+    //for now we only have the logout case (we could add more in the future)
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuProfile:
+
+                finish();
+                startActivity(new Intent(this, ProfileScreen.class));
+                break;
+
+            case R.id.menuLogout:
+
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(this, LoginScreen.class));
+
+                break;
+        }
+        return true;
     }
 }
