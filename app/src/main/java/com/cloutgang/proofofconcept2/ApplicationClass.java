608aef7e650526c8bfd7398e4c0fd91eddf6e997
Jader Vulcano <jvjader@gmail.com>
2019-01-13 18:17:24 +0100

    added multidexing because fuck optimization

---
+++ app/src/main/java/com/cloutgang/proofofconcept2/ApplicationClass.java
@@ -0,0 +1,14 @@
+package com.cloutgang.proofofconcept2;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by jvjad on 13-Jan-19.
+ */
+
+public class ApplicationClass extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
